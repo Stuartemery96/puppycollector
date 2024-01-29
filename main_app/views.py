fe155e30f77ae50fd70c0430afbe42df08c09d1e
stuartemery96 <stuemery96@gmail.com>
2024-01-29 14:09:58 -0800

    Lab 2 complete

--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,5 @@
 from django.shortcuts import render
-
-puppies = [
-  {'name': 'Hazel', 'breed': 'chocolate lab', 'description': 'Rambunctious little bird dog', 'age': '1'},
-  {'name': 'Remmy', 'breed': 'mini austrailian shephard', 'description': 'little fur ball', 'age': '5'},
-]
+from .models import Puppy
 
 # Create your views here.
 def home(request):
@@ -13,4 +9,9 @@ def about(request):
   return render(request, 'about.html')
 
 def puppies_index(request):
+  puppies = Puppy.objects.all()
   return render(request, 'puppies/index.html', {'puppies': puppies})
+
+def puppies_detail(request, puppy_id):
+  puppy = Puppy.objects.get(id=puppy_id)
+  return render(request, 'puppies/detail.html', { 'puppy': puppy})
